1d76da86cea041bccc18d4c611791e3b1fe151be
Phil Mickelson <phil@cbasoftware.com>
2020-06-30 19:58:06 -0400

    Updates

---
+++ precache-manifest.4191917e43862ba78c4f481a63360cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a17f2e139c29887eb81440a4afb744",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0c8e98d0cfac67ffd69e",
+    "url": "/monsters-rolodex/static/css/main.f7d5d5fa.chunk.css"
+  },
+  {
+    "revision": "b7025d703d6faac98ebe",
+    "url": "/monsters-rolodex/static/js/2.a0f84a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a0f84a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8e98d0cfac67ffd69e",
+    "url": "/monsters-rolodex/static/js/main.50ff9918.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
